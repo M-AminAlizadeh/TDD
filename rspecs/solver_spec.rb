a36aa7a2cb6f7dda52d44f5f6a034c7075cc00e4
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-10-13 13:33:49 +0330

    Add unit tests for fizzbuzz method in solve class

--- rspecs/solver_spec.rb
+++ rspecs/solver_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../solver'
 
 describe Solver do
-  let(:solver) {Solver.new}
+  let(:solver) { Solver.new }
 
   describe '#factorial' do
     it 'show the factorial of 5' do
@@ -11,9 +11,9 @@ describe Solver do
 
     it 'show the factorial of 11' do
       number = 11
-      expect(solver.factorial(number)).to eq(39916800)
+      expect(solver.factorial(number)).to eq(39_916_800)
     end
-    
+
     it 'show the factorial of 0' do
       number = 0
       expect(solver.factorial(number)).to eq(1)
@@ -29,6 +29,33 @@ describe Solver do
     it 'reverse winnie string passed to it' do
       word = 'winnie'
       expect(solver.reverse(word)).to eq('einniw')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'show fizz for the number 9' do
+      number = 9
+      expect(solver.fizzbuzz(number)).to eq('fizz')
+    end
+
+    it 'show fizz for the number 0' do
+      number = 0
+      expect(solver.fizzbuzz(number)).to eq('fizzbuzz')
+    end
+
+    it 'show fizz for the number 15' do
+      number = 15
+      expect(solver.fizzbuzz(number)).to eq('fizzbuzz')
+    end
+
+    it 'show fizz for the number 100' do
+      number = 100
+      expect(solver.fizzbuzz(number)).to eq('buzz')
+    end
+
+    it 'show fizz for the number 91' do
+      number = 91
+      expect(solver.fizzbuzz(number)).to eq('91')
     end
   end
 end
