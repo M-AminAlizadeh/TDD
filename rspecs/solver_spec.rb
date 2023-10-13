2e9d3fc18a74b366f0edcd649e49ba47cf48bf95
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-13 10:35:19 +0100

    Create tests for reverse method

--- rspecs/solver_spec.rb
+++ rspecs/solver_spec.rb
@@ -1,6 +1,17 @@
 require_relative '../solver'
 
 describe Solver do
-  
+  let(:solver) {Solver.new}
+    
+  describe '#reverse' do
+    it 'reverse hello string passed to it' do
+      word = 'hello'
+      expect(solver.reverse(word)).to eq('olleh')
+    end
 
+    it 'reverse winnie string passed to it' do
+      word = 'winnie'
+      expect(solver.reverse(word)).to eq('einniw')
+    end
+  end
 end
