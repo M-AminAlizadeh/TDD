dd1f2d3780a34c8710fe8aaaad2466ee152eecbd
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-10-13 13:17:06 +0330

    Add unit test to factorial method

--- rspecs/solver_spec.rb
+++ rspecs/solver_spec.rb
@@ -2,7 +2,24 @@ require_relative '../solver'
 
 describe Solver do
   let(:solver) {Solver.new}
+
+  describe '#factorial' do
+    it 'show the factorial of 5' do
+      number = 5
+      expect(solver.factorial(number)).to eq(120)
+    end
+
+    it 'show the factorial of 11' do
+      number = 11
+      expect(solver.factorial(number)).to eq(39916800)
+    end
     
+    it 'show the factorial of 0' do
+      number = 0
+      expect(solver.factorial(number)).to eq(1)
+    end
+  end
+
   describe '#reverse' do
     it 'reverse hello string passed to it' do
       word = 'hello'
